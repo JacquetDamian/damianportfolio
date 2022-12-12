fda706b3e2c9e29d2b2eae9cbc3b7e7e002941ab
Damián Jacquet <116528666+JacquetDamian@users.noreply.github.com>
2022-12-12 12:16:29 -0300

    Add files via upload
    Fix some bugs.

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,4 +1,4 @@
-const $form= document.querySelector("#form");
+/* const $form= document.querySelector("#form");
 const $buttonMailto = document.querySelector('#mailto')
 
 $form.addEventListener('submit', handleSubmit);
@@ -9,4 +9,4 @@ function handleSubmit (event) {
     console.log(form.get('name'));
     $buttonMailto.setAttribute('href', `mailto:damianjacquet9@gmail.com?subject${form.get('name')}${form.get('email')}&body=${form.get('message')}`);
     $buttonMailto.click();
-}
+} */
--- proyectos/script.js
+++ proyectos/script.js
@@ -1,15 +1,3 @@
-const $form= document.querySelector("#form");
-const $buttonMailto = document.querySelector('#mailto')
-
-$form.addEventListener('submit', handleSubmit);
-
-function handleSubmit (event) {
-    event.preventDefault();
-    const form = new FormData(this);
-    console.log(form.get('name'));
-    $buttonMailto.setAttribute('href', `mailto:damianjacquet9@gmail.com?subject${form.get('name')}${form.get('email')}&body=${form.get('message')}`);
-    $buttonMailto.click();
-}
 
 $(function(){
 
